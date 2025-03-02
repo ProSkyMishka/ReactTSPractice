48004dc1fcb7cfa655ca291def047dc27011a913
ProSkyMishka <145161036+ProSkyMishka@users.noreply.github.com>
2025-03-02 20:39:43 +0300

    Solve tak_1

--- task_1.ts
+++ task_1.ts
@@ -1,1 +1,26 @@
+enum Gender {
+    MALE = "Мужчина",
+    FEMALE = "Женщина"
+}
 
+interface Person {
+    name: string,
+    age: number,
+    hobbies: string[],
+    genser: Gender
+}
+
+function personToString(person: Person): string {
+    return `${person.name}, ${person.age} лет, любит: ${person.hobbies.join(", ")}. Пол: ${person.genser}`
+}
+
+
+// test
+const test: Person = {
+    name: "test",
+    age: 10,
+    hobbies: ["test1", "test2"],
+    genser: Gender.MALE
+}
+
+console.log(personToString(test))
